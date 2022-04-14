7ab459b6fb9967c03fcbcbb2adbb88ff7644a75f
Eunyeol <ney9083@gmail.com>
2022-04-14 19:30:43 +0900

    Feat: SPA -vanilla js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(
+  "/static",
+  express.static(path.resolve(__dirname, "frontend", "static"))
+);
+// 브라우저 새로고침을 위한 처리 (다른 route가 존재하는 경우 맨 아래에 위치해야 한다)
+// 브라우저 새로고침 시 서버는 index.html을 전달하고 클라이언트는 window.location.pathname를 참조해 다시 라우팅한다.
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend/index.html"));
+});
+
+app.listen(process.env.PORT || 5080, () => console.log("Server running..."));
